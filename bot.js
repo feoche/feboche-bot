778737b5feeee956e6ad086a70e1c6ac0091b484
feoche <francois.eoche@corp.ovh.com>
2018-04-04 18:01:20 +0200

    Up

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ function streamCallback (stream) {
 
         console.info(
           "\x1b[36m", ("[" + tweet.user.screen_name + "]").padStart(15),                                   // User
-          "\x1b[34m", ("[" + followers + "f - " + ((1 / probability) * 100).toFixed(0) + "%]").padEnd(10), // Followers + Probability
+          "\x1b[34m", ("[" + followers + "f-" + ((1 / probability) * 100).toFixed(0) + "%]").padEnd(10), // Followers + Probability
           "\x1b[0m", (tweet.text.replace('\n', '')).padEnd(140)                                            // Title
         );
 
