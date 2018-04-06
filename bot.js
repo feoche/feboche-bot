93583a7f8d0a13d84a452f63416a5b595bb7fbac
feoche <francois.eoche@corp.ovh.com>
2018-04-06 14:40:41 +0200

    logs

--- bot.js
+++ bot.js
@@ -111,7 +111,7 @@ function streamCallback (stream) {
 }
 
 function onStreamError (err, code) {
-  console.error(`Error (${code} - ${err}) - Reloading...`)
+  console.error(`Error (${code} - ${JSON.stringify(err)}) - Reloading...`)
   setTimeout(initStreaming, 5000)
 }
 
