01b4e0a1ce1348a9a50e015f423279751c49d68c
feoche <francois.eoche@corp.ovh.com>
2018-06-14 13:58:17 +0200

    Yay- !

--- bot.js
+++ bot.js
@@ -104,7 +104,7 @@ function streamCallback (stream) {
                 socket.emit('new', {
                   severity: 1,
                   text: tweet.text.replace('\n', '').trim().replace(/(\r\n\t|\n|\r\t)/gm, '').replace(/\shttp.*/g, ''),
-                  url: (tweet.entities && tweet.entities.urls && tweet.entities.urls[0] && tweet.entities.urls[0].url) || 'http://' + text.split(/http/)[text.split(/http/).length - 1] || ''
+                  url: (tweet.entities && tweet.entities.urls && tweet.entities.urls[0] && tweet.entities.urls[0].url) || 'http://' + tweet.text.split(/http/)[tweet.text.split(/http/).length - 1] || ''
                 })
                 sent = true
                 // Reset number of tweets
@@ -122,7 +122,7 @@ function streamCallback (stream) {
       socket.emit('new', {
         severity: 0,
         text: tweet.text.replace('\n', '').trim().replace(/(\r\n\t|\n|\r\t)/gm, '').replace(/\shttp.*/g, ''),
-        url: (tweet.entities && tweet.entities.urls && tweet.entities.urls[0] && tweet.entities.urls[0].url) || 'http://' + text.split(/http/)[text.split(/http/).length - 1] || ''
+        url: (tweet.entities && tweet.entities.urls && tweet.entities.urls[0] && tweet.entities.urls[0].url) || 'http://' + tweet.text.split(/http/)[tweet.text.split(/http/).length - 1] || ''
       })
     }
   })
