841a665d3c4670993a3ff4c5ad4ed57f4fdaadab
feoche <francois.eoche@corp.ovh.com>
2018-06-21 17:41:14 +0200

    Up

--- bot.js
+++ bot.js
@@ -1,168 +1,61 @@
 import Twitter from 'twitter'
 import minimist from 'minimist'
-import LanguageDetect from 'languagedetect'
-import http from 'http'
-import express from 'express'
-import io from 'socket.io'
-import {data} from './data.js'
+import MarkovChain from 'markovchain'
 
 // Retrieve args
 const args = minimist(process.argv.slice(2))
 
-let userTweets = []
-let recordedTweets = []
-
-let lngDetector = new LanguageDetect();
+let userTweets = ''
 
 // create an object using the keys we just determined
-let twitterAPI = new Twitter({
-  consumer_key: process.env.CONSUMER_TOKEN || args.consumer_key,
-  consumer_secret: process.env.CONSUMER_SECRET || args.consumer_secret,
-  access_token_key: process.env.ACCESS_TOKEN_KEY || args.access_token_key,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET || args.access_token_secret
+// let twitterAPI = new Twitter({
+//   consumer_key: process.env.CONSUMER_TOKEN || args.consumer_key,
+//   consumer_secret: process.env.CONSUMER_SECRET || args.consumer_secret,
+//   access_token_key: process.env.ACCESS_TOKEN_KEY || args.access_token_key,
+//   access_token_secret: process.env.ACCESS_TOKEN_SECRET || args.access_token_secret
+// })
+const twitterAPI = new Twitter({
+  consumer_key: '82QnFpIsTCOmAxhlSVrbWC8XU',
+  consumer_secret: 'gN999Z2fAyIpKQhi6H5V8zSx4AHMBsH2BTrtRtjiWcCmrCj1r7',
+  access_token_key: '1009809110248849409-2pzcXZSQ6Q087suplTsT2Y4yDe1S43',
+  access_token_secret: 'i6uiVmb6lgzU5OLcXB8mOsd2tA7QmWas0hFAhZ7Fc3Xbd'
 })
-let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
-let ipadr = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
-console.log(`Logged in`)
-initStreaming()
+const generateMarkov = string => {
+  // Filter tweets
+  string = string.replace(/\@\w+|(?:https?|ftp):\/\/[\n\S]+/g, '');
 
-function containsRegExp (text, array) {
-  let res = [];
-  for (let regexp of array) {
-    let match = text.match(regexp)
-    if (match) {
-      res.push(match);
-    }
+  const markov = new MarkovChain(string);
+
+  let newTweet = markov.end(30).process(); // Set the word limit to 30
+  // If the new lyrics are too short or are over Twitter's 280 characters limit, we just generate some new ones
+  if (newTweet.length < 20 || newTweet.length > 280) {
+    generateMarkov(string);
   }
-  return res.length && res || false;
-}
 
-function pickRand (data) {
-  return Math.floor(Math.random() * data)
-}
+  // Prettify the output
+  newTweet = newTweet.charAt(0).toUpperCase() + newTweet.slice(1);
+  newTweet = newTweet.replace(/([,!] )(\w)/g, (match, $1, $2) => {
+    return $1 + '\n' + $2.toUpperCase();
+  });
 
-function streamCallback (stream) {
-  console.log(`streaming`)
+  console.info('newTweet : ', newTweet);
+};
 
-  let server = http.createServer(express())
-  let socket = io.listen(server)
 
-  socket.on('connection', () => console.log('Launchpad connected'))
-
-  server.listen(port, ipadr)
-  console.log(port + ':' + ipadr)
-
-  stream.on(`data`, tweet => {
-    let sent = false
-
-    if (isAllowedTweet(tweet)) {
-      let result = ``
-      let userName = tweet.user && tweet.user.screen_name
-      let text = tweet.text
-      let followers = (tweet.user && tweet.user.followers_count) || 0
-
-      userTweets[userName] = {postedTweets: (userTweets[userName] && userTweets[userName].postedTweets || 0) + 1}
-      let probability =
-        Math.max(
-          data.MINPROBABILITY +
-          (followers - data.MINFOLLOWERS) /
-          (data.MAXFOLLOWERS - data.MINFOLLOWERS) *
-          (data.MAXPROBABILITY - data.MINPROBABILITY),
-          data.MAXPROBABILITY)
-
-      probability = Math.min(probability, probability / (userTweets[userName].postedTweets / 2))
-      let textLog = tweet.text.replace('\n', '').trim().replace(/(\r\n\t|\n|\r\t)/gm, '').replace(/\shttp.*/g, '').replace(/digital/g, '\x1b[96mdigital\x1b[0m')
-      console.info(
-        '\x1b[96m', ('[' + new Date().toLocaleTimeString() + ']').padStart(10),
-        '\x1b[94m', ('[@' + userName + ']').padStart(20),                                                                                                                                         // User
-        '\x1b[91m', ('[' + followers + 'f-' + ((1 / probability) * 100).toFixed(0) + '%]').padStart(15),                                                                                          // Followers + Probability
-        '\x1b[0m', textLog.padEnd(125),                                                                                                                                                           // Title
-        ((tweet.entities && tweet.entities.urls && tweet.entities.urls[0] && tweet.entities.urls[0].url) || 'http://' + text.split(/http/)[text.split(/http/).length - 1] || '').padEnd(40)   // URL
-      );
-
-      if (!pickRand(probability)) {
-        for (let item of data.PROHIBITEDWORDS) {
-          if (containsRegExp(text, item.queries)) {
-            result = item.responses[pickRand(item.responses.length)]
-          }
-        }
-
-        // Log
-        let tweetDone = `@${userName} ${result} \n${data.LINKS[pickRand(data.LINKS.length)]}`
-        console.log(`—> `, tweetDone.trim().replace(/(\r\n\t|\n|\r\t)/gm, ''))
-
-        if (!args.test) { // TWEET
-          twitterAPI.post('statuses/update', {
-              status: tweetDone.substring(0, data.MAXTWEETLIMIT),
-              in_reply_to_status_id: tweet.id_str
-            },
-            (error) => {
-              if (error) {
-                console.error('Error: ', error)
-              } else {
-                socket.emit('new', {
-                  severity: 2,
-                  text: tweet.text.replace('\n', '').trim().replace(/(\r\n\t|\n|\r\t)/gm, '').replace(/\shttp.*/g, ''),
-                  url: (tweet.entities && tweet.entities.urls && tweet.entities.urls[0] && tweet.entities.urls[0].url) || ''
-                })
-                sent = true
-                // Reset number of tweets
-                if (userTweets[userName]) {
-                  userTweets[userName].postedTweets = 0
-                  userTweets[userName].lastTweet = Date.now()
-                }
-              }
-            }
-          )
-        }
-      }
-      if (!sent) {
-        socket.emit('new', {
-          severity: 1,
-          text: tweet.text.replace('\n', '').trim().replace(/(\r\n\t|\n|\r\t)/gm, '').replace(/\shttp.*/g, ''),
-          url: (tweet.entities && tweet.entities.urls && tweet.entities.urls[0] && tweet.entities.urls[0].url) || ''
-        })
-        sent = true
-      }
+twitterAPI.get('statuses/user_timeline', {
+  'screen_name': 'feoche',
+  'count': '200',
+  'exclude_replies': true,
+  'include_rts': false
+}, (err, data) => {
+  if(!err) {
+    for (let i = 0; i < data.length; i++) {
+      userTweets += data[i].text + '\n';
     }
-    if (!sent) {
-      socket.emit('new', {
-        severity: 0,
-        text: tweet.text.replace('\n', '').trim().replace(/(\r\n\t|\n|\r\t)/gm, '').replace(/\shttp.*/g, ''),
-        url: (tweet.entities && tweet.entities.urls && tweet.entities.urls[0] && tweet.entities.urls[0].url) || ''
-      })
-    }
-  })
-  // if something happens, call the onStreamError function
-  stream.on(`end`, onStreamError)
-  stream.on(`error`, onStreamError)
-}
-
-function onStreamError (err) {
-  console.error(`Error (${err}) - Reloading...`)
-  setTimeout(initStreaming, 10000)
-}
-
-function initStreaming () {
-  // initialize the stream and everything else
-  twitterAPI.stream(
-    `statuses/filter`,
-    {
-      track: data.SEARCHWORDS.join(`,`),
-      language: `fr`
-    },
-    streamCallback
-  )
-}
-
-function isAllowedTweet (tweet) {
-  return tweet.text &&                                                                                                                             // Tweet contains text
-    tweet.lang === `fr` &&                                                                                                                         // Tweet is in french
-    lngDetector.detect(tweet.text) && lngDetector.detect(tweet.text)[0] && lngDetector.detect(tweet.text)[0][0] === 'french' &&                    // Tweet is in french for sure
-    containsRegExp(tweet.text, data.PROHIBITEDWORDS[0].queries) &&                                                                                 // Tweet contains `prohibited` words
-    !containsRegExp(tweet.text, data.EXCEPTIONS) &&                                                                                                // Tweet doesn`t contain any of the excluded terms
-    !containsRegExp(tweet.text, [/RT\s\@/]) &&                                                                                                     // Tweet is not a Retweet
-    !recordedTweets.includes(tweet.text) &&                                                                                                        // Tweet has not been already answered
-    tweet.retweeted_status === undefined                                                                                                           // If bot has not already tweeted this
-}
+    generateMarkov(userTweets);
+    setInterval(() => {
+      generateMarkov(userTweets);
+    }, 10000);
+  }
+})
