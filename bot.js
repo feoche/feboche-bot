511047cb0348b05906bb9a040fe7eef65cde5ca7
feoche <francoiseoche@hotmail.com>
2018-06-21 20:49:20 +0200

    UP

--- bot.js
+++ bot.js
@@ -1,11 +1,12 @@
-import Twitter from 'twitter'
-import minimist from 'minimist'
-import MarkovChain from 'markovchain'
+import Twitter from 'twitter';
+import minimist from 'minimist';
+import MarkovChain from 'markovchain';
+import { data } from './data.js';
 
 // Retrieve args
-const args = minimist(process.argv.slice(2))
+const args = minimist(process.argv.slice(2));
 
-let userTweets = ''
+let userTweets = '';
 
 // create an object using the keys we just determined
 // let twitterAPI = new Twitter({
@@ -19,7 +20,7 @@ const twitterAPI = new Twitter({
   consumer_secret: 'gN999Z2fAyIpKQhi6H5V8zSx4AHMBsH2BTrtRtjiWcCmrCj1r7',
   access_token_key: '1009809110248849409-2pzcXZSQ6Q087suplTsT2Y4yDe1S43',
   access_token_secret: 'i6uiVmb6lgzU5OLcXB8mOsd2tA7QmWas0hFAhZ7Fc3Xbd'
-})
+});
 
 const generateMarkov = string => {
   // Filter tweets
@@ -36,26 +37,37 @@ const generateMarkov = string => {
   // Prettify the output
   newTweet = newTweet.charAt(0).toUpperCase() + newTweet.slice(1);
   newTweet = newTweet.replace(/([,!] )(\w)/g, (match, $1, $2) => {
-    return $1 + '\n' + $2.toUpperCase();
+    return `${$1}\n${$2.toUpperCase()}`;
   });
 
-  console.info('newTweet : ', newTweet);
-};
-
-
-twitterAPI.get('statuses/user_timeline', {
-  'screen_name': 'feoche',
-  'count': '200',
-  'exclude_replies': true,
-  'include_rts': false
-}, (err, data) => {
-  if(!err) {
-    for (let i = 0; i < data.length; i++) {
-      userTweets += data[i].text + '\n';
+  twitterAPI.post('statuses/update', {
+      status: newTweet.substring(0, 280)
+    },
+    error => {
+      if (error) {
+        console.error('Error: ', error);
+      }
     }
-    generateMarkov(userTweets);
-    setInterval(() => {
-      generateMarkov(userTweets);
-    }, 10000);
-  }
-})
+  );
+};
+generateMarkov(data.join('\n'));
+setInterval(() => {
+  generateMarkov(data.join('\n'));
+}, 1000 * 60 * 60 * 24);
+
+// twitterAPI.get('statuses/user_timeline', {
+//   'screen_name': 'feoche',
+//   'count': '200',
+//   'exclude_replies': true,
+//   'include_rts': false
+// }, (err, data) => {
+//   if(!err) {
+//     for (let i = 0; i < data.length; i++) {
+//       userTweets += data[i].text + '\n';
+//     }
+//     generateMarkov(userTweets);
+//     setInterval(() => {
+//       generateMarkov(userTweets);
+//     }, 10000);
+//   }
+// })
